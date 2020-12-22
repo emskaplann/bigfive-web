d0d730ccf48857b7e06208f09f9e6e5bb6328344
Emirhan Kaplan <emskaplann@gmail.com>
2020-12-22 09:50:51 -0500

    konu basliklarinin aciklamalari eklendi.

--- pages/agreeableness.vue
+++ pages/agreeableness.vue
@@ -12,70 +12,24 @@
     </v-img>
     <v-card-text>
       <p>
-        Agreeableness reflects individual differences in concern with cooperation and social harmony. Agreeable individuals value getting along with others.
-      </p>
-      <p>
-        They are therefore considerate, friendly, generous, helpful, and willing to compromise their interests with others'.
-        Agreeable people also have an optimistic view of human nature.
-        They believe people are basically honest, decent, and trustworthy.
-        Disagreeable individuals place self-interest above getting along with others.
-        They are generally unconcerned with others' well-being, and therefore are unlikely to extend themselves for other people.
-        Sometimes their skepticism about others' motives causes them to be suspicious, unfriendly, and uncooperative.
-      </p>
-
-      <blockquote class="blockquote">
-        <v-icon>{{ mdiFormatQuoteOpen }}</v-icon>
-        Agreeableness reflects individual differences in concern with cooperation and social harmony.
-        <v-icon>{{ mdiFormatQuoteClose }}</v-icon>
-      </blockquote>
-
-      <p>
-        Agreeableness is obviously advantageous for attaining and maintaining popularity.
-        Agreeable people are better liked than disagreeable people.
-        On the other hand, agreeableness is not useful in situations that require tough or absolute objective decisions.
-        Disagreeable people can make excellent scientists, critics, or soldiers.
-      </p>
-
-      <div class="title">
-        Trust
-      </div>
-      <p>
-        A person with high trust assumes that most people are fair, honest, and have good intentions. Persons low in trust see others as selfish, devious, and potentially dangerous.
-      </p>
-
-      <div class="title">
-        Morality
-      </div>
-      <p>
-        High scorers on this scale see no need for pretense or manipulation when dealing with others and are therefore candid, frank, and sincere. Low scorers believe that a certain amount of deception in social relationships is necessary. People find it relatively easy to relate to the straightforward high-scorers on this scale. They generally find it more difficult to relate to the unstraightforward low-scorers on this scale. It should be made clear that low scorers are not unprincipled or immoral; they are simply more guarded and less willing to openly reveal the whole truth.
-      </p>
-
-      <div class="title">
-        Alturism
-      </div>
-      <p>
-        Altruistic people find helping other people genuinely rewarding. Consequently, they are generally willing to assist those who are in need. Altruistic people find that doing things for others is a form of self-fulfillment rather than self-sacrifice. Low scorers on this scale do not particularly like helping those in need. Requests for help feel like an imposition rather than an opportunity for self-fulfillment.
-      </p>
-
-      <div class="title">
-        Cooperation
-      </div>
-      <p>
-        Individuals who score high on this scale dislike confrontations. They are perfectly willing to compromise or to deny their own needs in order to get along with others. Those who score low on this scale are more likely to intimidate others to get their way.
-      </p>
-
-      <div class="title">
-        Modesty
-      </div>
-      <p>
-        High scorers on this scale do not like to claim that they are better than other people. In some cases this attitude may derive from low self-confidence or self-esteem. Nonetheless, some people with high self-esteem find immodesty unseemly. Those whoare willing to describe themselves as superior tend to be seen as disagreeably arrogant by other people.
-      </p>
-
-      <div class="title">
-        Sympathy
-      </div>
-      <p>
-        People who score high on this scale are tenderhearted and compassionate. They feel the pain of others vicariously and are easily moved to pity. Low scorers are not affected strongly by human suffering. They pride themselves on making objective judgments based on reason. They are more concerned with truth and impartial justice than with mercy.
+        Bu boyut, bireyin kişisel yönelme düzeyini etkilemektedir. Uyumlu insan arkadaşça davranır, birlikte çalışmayı sever, 
+        kibardır, hoşgörü sınırı geniştir, güven vericidir ve yumuşak kalplidir. Bu tip yöneticiler astlarını iyi motive eder, 
+        onların ihtiyaçlarını gidermeye yönelik çalışırlar ve iyi iletişim kurarlar. Düşük uyumluluğa sahip kişiler ise diğerlerinin 
+        maksatlarında şüpheci ve işbirlikçilik yerine rakip olmayı tercih eden kişilerdir . Yumuşak başlılık boyutunda yüksek puan 
+        alanlar yumuşak kalpli, iyi huylu, güvenli, yardımsever, bağışlayıcı, saf ve dürüst olarak tanımlandı. Bu boyutta düşük puan 
+        alan kişiler kötümser, kaba, güvensiz, işbirliğine kapalı, intikamcı, merhametsiz, sinirli ve çıkarcı olarak tanımlandı. 
+        Yumuşak başlılık daha az kişiler arası çatışmaya daha fazla iş ve aile çatışmasını azaltıcı desteğe neden olur. Şu halde, 
+        yumuşak başlılıkla çatışmanın da negatif bağıntılı olması beklenebilir Kabul edici bireyler işlerinde daha başarılıdır ve 
+        iş arkadaşları veya ailelerinden daha fazla duygusal destek görürler . Dede’ye  göre uyumluluk, diğer insanlarla makbul 
+        ölçüde anlaşmak ve memnuniyet verici ve hoşnut edici ilişkiler kurabilme becerisidir. Ülkemizde gerçekleştirilen çalışmalarda 
+        yumuşak başlılık boyutu adı altında tanımlanan kişilik özelliklerinin, Türk dilindeki kişilik özelliklerini tanımlayan 
+        sıfatlarla karşılaştırdığı zaman bu boyutla oldukça bağlantılı faktörler bulunduğunu görmekteyiz (. Araştırma sonuçları da 
+        göstermektedir ki, duygusal zeka, akıl sağlığı, psikolojik iyi olma ve yaşam doyumu ile pozitif ilişki göstermektedir. 
+        Bireyin duygularındaki düzensizlik Üniversite Öğrencilerinin Beş Faktör Kişilik Kuramı’na Göre Kişilik Özellikleri Alt 
+        Boyutlarının Bazı Değişkenlere Göre İncelenmesi kişilerarası ilişkilerin bozulmasına, kaygı artışına ve ruhsal bozukluklara 
+        neden olur ve yaşam doyumunu olumsuz etkilemektedir. İnsanlar duygularını diğer bireylerle paylaştığı, hissettiği ve sosyal 
+        ilişkilere girdiği sürece mutlu olur psikolojik doyum sağlar. Kişi benliğini, kişiliğini tanıdıkça ve düşünceleri ile 
+        duygularının farkında oldukça, bu doyum artacak ve yaşamın diğer alanlarını da olumlu etkileyecektir.
       </p>
     </v-card-text>
   </v-card>
--- pages/extraversion.vue
+++ pages/extraversion.vue
@@ -12,79 +12,32 @@
     </v-img>
     <v-card-text>
       <p>
-        Extraversion is marked by pronounced engagement with the external world.
-      </p>
-      <p>
-        Extraverts enjoy being with people, are full of energy, and often experience positive emotions.
-        They tend to be enthusiastic, action-oriented, individuals who are likely to say "Yes!" or "Let's go!" to opportunities for excitement.
-        In groups they like to talk, assert themselves, and draw attention to themselves.
-      </p>
-      <blockquote class="blockquote">
-        <v-icon>{{ mdiFormatQuoteOpen }}</v-icon>
-        Extraversion is marked by pronounced engagement with the external world.
-        <v-icon>{{ mdiFormatQuoteClose }}</v-icon>
-      </blockquote>
-      <p>
-        Introverts lack the exuberance, energy, and activity levels of extraverts.
-        They tend to be quiet, low-key, deliberate, and disengaged from the social world.
-        Their lack of social involvement should not be interpreted as shyness or depression; the introvert simply needs less stimulation than an extravert and prefers to be alone.
-        The independence and reserve of the introvert is sometimes mistaken as unfriendliness or arrogance.
-        In reality, an introvert who scores high on the extraversion dimension will not seek others out but will be quite pleasant when approached.
-      </p>
-
-      <div class="title">
-        Friendliness
-      </div>
-      <p>
-        Friendly people genuinely like other people and openly demonstrate positive feelings toward others.
-        They make friends quickly and it is easy for them to form close, intimate relationships.
-        Low scorers on Friendliness are not necessarily cold and hostile, but they do not reach out to others and are perceived as distant and reserved.
-      </p>
-
-      <div class="title">
-        Gregariousness
-      </div>
-      <p>
-        Gregarious people find the company of others pleasantly stimulating and rewarding.
-        They enjoy the excitement of crowds. Low scorers tend to feel overwhelmed by, and therefore actively avoid, large crowds.
-        They do not necessarily dislike being with people sometimes, but their need for privacy and time to themselves is much greater than for individuals who score high on this scale.
-      </p>
-
-      <div class="title">
-        Assertiveness
-      </div>
-      <p>
-        High scorers Assertiveness like to speak out, take charge, and direct the activities of others.
-        They tend to be leaders in groups.
-        Low scorers tend not to talk much and let others control the activities of groups.
-      </p>
-
-      <div class="title">
-        Activity Level
-      </div>
-      <p>
-        Active individuals lead fast-paced, busy lives.
-        They move about quickly, energetically, and vigorously, and they are involved in many activities.
-        People who score low on this scale follow a slower and more leisurely, relaxed pace.
-      </p>
-
-      <div class="title">
-        Excitement-Seeking
-      </div>
-      <p>
-        High scorers on this scale are easily bored without high levels of stimulation.
-        They love bright lights and hustle and bustle.
-        They are likely to take risks and seek thrills.
-        Low scorers are overwhelmed by noise and commotion and are adverse to thrill-seeking.
-      </p>
-
-      <div class="title">
-        Cheerfulness
-      </div>
-      <p>
-        This scale measures positive mood and feelings, not negative emotions (which are a part of the Neuroticism domain).
-        Persons who score high on this scale typically experience a range of positive feelings, including happiness, enthusiasm, optimism, and joy.
-        Low scorers are not as prone to such energetic, high spirits.
+        Bu boyut, kişilerin ne kadar sosyal, aktif, kararlı, konuşkan ve girişgen olduğunu, ayrıca insanları ve büyük grupları ne 
+        kadar sevdiklerini göstermektedir. Fazla dışa dönük tipler genellikle mutlu, enerji dolu, sempatik ve sevimli kişilerdir. 
+        Düşük dışa dönük tipler ise, genellikle bu özellikleri göstermezler ama asosyal kişiler de değildirler. Bir grup 
+        araştırmacının gözlemlediğine göre, “içedönükler soğuk değil çekingen, izleyici değil bağımsız, uyuşuk değil ağır adımlı 
+        insanlardır”. Dışa dönük kişiler atılgan, aktif, konuşkan, şen şakrak, iyimser ve enerjiktirler; eğlence ve hareketten 
+        hoşlanırlar. Dışadönüklerin pozitif ve enerjik özellikleri daha çok çatışma ve kolaylıklarla ilgilidir. Daha yüksek 
+        enerji potansiyeli sayesinde, dışadönükler verilen zamanda daha fazla tasarı üretebilir ve içedönüklere göre daha az 
+        yorgunluk hissi duyabilirler. Araştırmacılara göre, dışadönükler daha fazla pozitif etkiye, daha fazla pozitif olay 
+        beklentisine ve daha güçlü tepkiye ve içedönüklerden daha fazla enerjiye sahiptirler Olumlu duygularla dışadönüklük 
+        arasındaki ilişkiyi doğrulayan araştırmalar Beş Temel Kişilik faktörünün kapsamını netleştirmektedir. Neşeli, istekli, 
+        arkadaşça gibi enerjik olumlu duyguların ağırlıklı olarak dışadönüklük ve ikincil olarak da yumuşak Üniversite Öğrencilerinin 
+        Beş Faktör Kişilik Kuramı’na Göre Kişilik Özellikleri Alt Boyutlarının Bazı Değişkenlere Göre İncelenmesi 
+        başlılık/uzlaşılabilirlik faktörlerinden yük alırken, cömert ve sıcak gibi daha ılımlı, olumlu duyguların, ağırlıklı 
+        olarak yumuşak başlılıktan ve ikincil olarak dışadönüklükten yük aldıklarını belirtmektedirler. Eysenck’e göre, dışadönük 
+        bireyler, sosyal, çeşitli gruplara ve eğlence partilerine katılmayı seven, çok sayıda arkadaşa sahip olan, 
+        kendi başlarına kalmayı, okumayı ve yalnız ders çalışmayı sevmeyen kişilerdir. Heyecan ve coşkuyu tercih eden, tehlikeli 
+        durumlara atılmaktan çekinmeyen, ani karar verip atak davranabilen, şaka yapmaktan ve konuşmaktan hoşlanan kimselerdir. 
+        Kolay iletişim kurarlar, kolay arkadaş edinirler, geçimli, iyimser, gülmeyi güldürmeyi seven insanlardır. Genellikle 
+        değişiklikten hoşlanırlar, kaygısız, kayıtsız ve sıkıntısızdırlar fakat kolay sinirlenip kendini kaybedip, duygularını 
+        kontrol etmede ve kendilerini engellemede her zaman başarılı değildirler. İçedönük kişiliğe sahip olan bireyler ise, daha 
+        çok kendi başlarına kalmayı tercih eden, çekingen, mahcup, sessiz, ciddi, dış gerçeklikten çok iç gerçekliğe yönelimli, 
+        içe kapanık iç gözlem yaparak kendi kendilerini inceleyen, insanlardan çok kitaplardan hoşlanan, çok yakın ve özel 
+        arkadaşları dışında diğer insanlara karşı mesafeli davranan, ihtiyatlı kimselerdir. Bu insanlar ilerisi için plan yapmayı, 
+        konuşmadan önce düşünmeyi, harekete geçmeden önce enine boyuna düşünmeyi, heyecanla yapılan ani hareketlerden kaçınmayı 
+        tercih ederler. Heyecanı ve coşkuyu sevmezler. Günlük yaşamın olağan sorunlarını çok ciddiye alır ve düzenli yaşamdan 
+        hoşlanırlar.
       </p>
     </v-card-text>
   </v-card>
