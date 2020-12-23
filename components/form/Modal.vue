295a3a3ab8de64d61e661e4e18a0bf8073a92acb
Emirhan Kaplan <emskaplann@gmail.com>
2020-12-22 21:03:18 -0500

    modala biraz susleme eklendi.

--- components/form/Modal.vue
+++ components/form/Modal.vue
@@ -17,29 +17,33 @@
 
       <v-card>
         <v-card-title class="headline grey lighten-2">
-          <b>Yılbaşı Özel Çekilişi!</b>
+          <b>🎄 Yılbaşı Özel Çekilişi!</b>
         </v-card-title>
 
         <v-card-text>
           <br>
-          Yeni yılda herkes kendini daha iyi tanısın, yeni yıl daha pozitif olsun diye #Kendinitanı olarak bol hediyeli bir çekiliş gerçekleştiriyoruz.
-          Sadece bağlantıdan sitemize girin BİG5 kişilik testini çözün hem kişilik raporunuzda kendinizi keşfedin hem de 2021’e renk katacak hediyelerimizi kazanma şansı yakalayın.
+          Yeni yılda herkes kendini daha iyi tanısın, yeni yıl daha pozitif olsun diye #KendiniTanı olarak bol hediyeli bir çekiliş gerçekleştiriyoruz.
+          Sadece bağlantıdan sitemize girin Big Five Kişilik Testini çözün hem kişilik raporunuzda kendinizi keşfedin hem de 2021’e renk katacak hediyelerimizi kazanma şansı yakalayın.
           <br><br>
           <b>YENİ YIL HEDİYELERİMİZ:</b>
-          20 KİŞİYE 200TL DEĞERİNDE AMAZON GİFTCARD
-          80 KİŞİYE PSİKOLOJİ KİTAPLARI.
+          <br>
+          🎁 20 KİŞİYE 200TL DEĞERİNDE AMAZON GİFTCARD
+          <br>
+          🎁 80 KİŞİYE PSİKOLOJİ KİTAPLARI
           <br><br>
           <b>ŞARTLAR:</b>
-          LİNKE TIKLADIĞIN TWEETİ RETWEET ETMEK,
-          KİŞİLİK TESTİNİ TAMAMLAMAK.
+          <br>
+          -LİNKE TIKLADIĞIN TWEETİ RETWEET ETMEK
+          <br>
+          -KİŞİLİK TESTİNİ TAMAMLAMAK
           <br><br>
-          “KAZANANLAR GENEL KAPSAMDA DUYURULACAK EK OLARAK ÖZELDEN İLETİŞİME GEÇİLECEKTİR”
+          “KAZANANLAR GENEL KAPSAMDA DUYURULACAK, EK OLARAK ÖZELDEN İLETİŞİME GEÇİLECEKTİR”
           <br><br>
           Lütfen şahsi ve aktif olarak kullandığınız Twitter Adresinizi giriniz, kazananlarla verdikleri Twitter Adresi üzerinden iletişime geçilecektir.
           <br><br>
           Kişilik testi sonucunun tutarlı olması için lütfen özveri ile çözünüz.
           <br><br>
-          <b>Son Katılım Tarihi:</b> 05/01/2021
+          <b>Son Katılım Tarihi:</b> <u>05/01/2021</u>
         </v-card-text>
 
         <v-divider />
